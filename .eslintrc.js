b6fc398f2a34320ce0932565aa9d2b8d8786ee8d
crystalclearhouse-data <crystalclearhouse@icloud.com>
2026-01-22 09:04:31 -0800

    chore(tk22): enforce core import boundaries via lint rules

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['src/tk22/core/**/*.{ts,tsx}'],
+      rules: {
+        'no-restricted-imports': [
+          'error',
+          {
+            patterns: [
+              '../adapters/*',
+              '../../adapters/*',
+              '../services/*',
+              '../../services/*',
+              '../agent/*',
+              '../../agent/*',
+              '../apis/*',
+              '../../apis/*',
+              '../gen/*',
+              '../../gen/*',
+              '../utils/*',
+              '../../utils/*',
+            ],
+          },
+        ],
+      },
+    },
+  ],
+};
